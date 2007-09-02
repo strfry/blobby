4c295dedaa8af7059e964e2e32887d2fdb78de3e
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2007-09-02 16:27:21 +0000

    Stripped some unnecessary stuff from raknet

--- src/raknet/LinuxStrings.h
+++ src/raknet/LinuxStrings.h
@@ -2,11 +2,9 @@
 #define _GCC_WIN_STRINGS
 
 #if (defined(__GNUC__)  || defined(__GCCXML__))
-int _stricmp(const char* s1, const char* s2);
-int _strnicmp(const char* s1, const char* s2, size_t n);
-//#ifndef __CYGWIN__
-//int _unlink(const char* filename);
-//#endif
+
+#define _stricmp strcasecmp
+#define _strnicmp strncasecmp
 
 #endif
 #endif
