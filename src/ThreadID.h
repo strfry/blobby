d0bff837ea71bf268bb5782bebf9005ff42707a6
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2011-12-01 22:27:44 +0000

    added ThreadID

---
+++ src/ThreadID.h
@@ -0,0 +1,55 @@
+/*=============================================================================
+Blobby Volley 2
+Copyright (C) 2008 Jonathan Sieber (jonathan_sieber@yahoo.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#pragma once
+
+class BlobbyThread;
+
+/*! \class ThreadID
+	\brief identifying a thread
+	\details This class can identify a thread, either with ID or with BlobbyThread pointer.
+			Additionally, more generic IDs are possible (such as all, none, current etc)
+	\todo check on construction time if thread really exists. turn this class into a kind of 
+			safe pointer to prevent access on deleted threads.
+*/
+class ThreadID
+{
+	struct GTI {};
+	public:
+		static GTI t_all;
+		static GTI t_none;
+		static GTI t_current;
+	
+	/// \todo implement this!
+	ThreadID( const GTI& = t_none );
+	/// construct from BlobbyThread
+	ThreadID(const BlobbyThread* t);
+	
+	/// construct from ID
+	ThreadID(unsigned int);
+	
+	// get ID representations
+	
+	const BlobbyThread* getThread() const;
+	unsigned int getThreadID() const;
+	
+	private:
+		const BlobbyThread* mThread;
+};
+
