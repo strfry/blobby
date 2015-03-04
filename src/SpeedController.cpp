91ed4a1941ca3f0494954ad3d4f9e226dd5cfc3a
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2015-03-04 23:17:17 +0000

    bugfix in speed controller

--- src/SpeedController.cpp
+++ src/SpeedController.cpp
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /* implementation */
 /// this is required to reduce rounding errors. now we have a resolution of
-/// 1µs. This is much better than SDL_Delay can handle, but we prevent 
+/// 1µs. This is much better than SDL_Delay can handle, but we prevent
 /// accumulation errors.
 const int PRECISION_FACTOR = 1000;
 
@@ -55,9 +55,9 @@ void SpeedController::setGameSpeed(float fps)
 	if (fps < 5)
 		fps = 5;
 	mGameFPS = fps;
-	
+
 	/// \todo maybe we should reset only if speed changed?
-	mBeginSecond = mOldTicks;
+	mBeginSecond = SDL_GetTicks();
 	mCounter = 0;
 }
 
@@ -69,7 +69,7 @@ bool SpeedController::doFramedrop() const
 void SpeedController::update()
 {
 	int rateTicks = std::max( static_cast<int>(PRECISION_FACTOR * 1000 / mGameFPS), 1);
-	
+
 	static int lastTicks = SDL_GetTicks();
 
 	if (mCounter == mGameFPS)
@@ -92,7 +92,7 @@ void SpeedController::update()
 		if (wait > 0)
 			SDL_Delay(wait);
 	}
-	
+
 	// do we need framedrop?
 	// if passed time > time when we should have drawn next frame
 	// maybe we should limit the number of consecutive framedrops?
